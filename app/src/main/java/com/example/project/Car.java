55c725520c6bb0b194c946d10e47d01ed1ed0c1f
Max Rapp <max.rapp01@live.se>
2023-05-22 22:25:04 +0200

    Car item class

--- app/src/main/java/com/example/project/Car.java
+++ app/src/main/java/com/example/project/Car.java
@@ -1,2 +1,34 @@
-package com.example.project;public class Car {
+package com.example.project;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Car {
+    @SerializedName("ID")
+    private String licensplate;
+    private String name;
+    @SerializedName("company")
+    private String speed;
+    @SerializedName("location")
+    private String color;
+    private String category;
+    @SerializedName("auxdata")
+    private String doors;
+
+    public Car(String licensplate, String name, String speed, String color, String category, String doors) {
+        this.licensplate = licensplate;
+        this.name = name;
+        this.speed = speed;
+        this.color = color;
+        this.category = category;
+        this.doors = doors;
+    }
+
+    public String getCar() {
+        return "Licensplate: " + licensplate +
+                "\nName: " + name +
+                "\nSpeed: " + speed +
+                "\nColor: " + color +
+                "\nCategory: " + category +
+                "\nDoors: " + doors;
+    }
 }
