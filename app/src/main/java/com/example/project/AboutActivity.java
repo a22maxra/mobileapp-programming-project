259cc5e411c3497d3cfafdeaa7ef6754da5b24ff
Max Rapp <max.rapp01@live.se>
2023-05-23 00:06:48 +0200

    Back button on about page

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button myButton = findViewById(R.id.buttonBack);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to launch the about activity
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+
+                // Launch the second activity
+                startActivity(intent);
+            }
+        });
     }
 }
