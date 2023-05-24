fef56a4add227b8978af86b6e8e5d266d7509623
Max Rapp <max.rapp01@live.se>
2023-05-24 17:14:03 +0200

    Sorting and reset

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -18,6 +18,8 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
@@ -34,6 +36,18 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Car item) {
+                Intent intent = new Intent(MainActivity.this, DetailView.class);
+                intent.putExtra("details", item.getCar());
+                startActivity(intent);
+            }
+        });
+        RecyclerView view = findViewById(R.id.displayCars);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+        new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=a22maxra");
 
         Button myButton = findViewById(R.id.buttonAbout);
         myButton.setOnClickListener(new View.OnClickListener() {
@@ -47,19 +61,51 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
             }
         });
 
-        adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+        Button buttonAsc = findViewById(R.id.buttonAsc);
+        buttonAsc.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(Car item) {
-                Intent intent = new Intent(MainActivity.this, DetailView.class);
-                intent.putExtra("details", item.getCar());
-                startActivity(intent);
+            public void onClick(View v) {
+                // Sort the items list by name ascending
+                Collections.sort(items, new Comparator<Car>() {
+                    @Override
+                    public int compare(Car car1, Car car2) {
+                        return car1.getName().compareTo(car2.getName());
+                    }
+                });
+
+                // Notify the adapter of the data change
+                adapter.notifyDataSetChanged();
             }
         });
-        RecyclerView view = findViewById(R.id.displayCars);
-        view.setLayoutManager(new LinearLayoutManager(this));
-        view.setAdapter(adapter);
-        new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=a22maxra");
+
+        Button buttonDesc = findViewById(R.id.buttonDesc);
+        buttonDesc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sort the items list by name descending
+                Collections.sort(items, new Comparator<Car>() {
+                    @Override
+                    public int compare(Car car1, Car car2) {
+                        return car2.getName().compareTo(car1.getName());
+                    }
+                });
+
+                // Notify the adapter of the data change
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        Button buttonReset = findViewById(R.id.buttonReset);
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask(MainActivity.this).execute("https://mobprog.webug.se/json-api?login=a22maxra");
+            }
+        });
+
     }
+
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
